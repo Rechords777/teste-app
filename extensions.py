e913ab49a3a4d0b582851575ce06339a8010b1b6
Rechords777 <crs57008@gmail.com>
2025-05-13 13:49:13 -0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from flask_migrate import Migrate
+
+# Extensões desacopladas para evitar importações circulares
+db = SQLAlchemy()
+migrate = Migrate()
+socketio = SocketIO()
